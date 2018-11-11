a1561d95893c05733ba90bd68c365b2846e961fa
lameck <lameck2@hotmail.com>
2018-11-11 16:33:59 -0300

    atualizando coisas

---
+++ src/TodoList.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import * as todoActions from "./actions/todos";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log(props);
+  }
+
+  state = {
+    newTodoText: ""
+  };
+
+  addNewTodo = () => {
+    this.props.addTodo(this.state.newTodoText);
+
+    this.setState({ newTodoText: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Testando teclado</li>
+        </ul>
+
+        <input
+          type="text"
+          value={this.state.newTodoText}
+          onChange={e => this.setState({ newTodoText: e.target.value })}
+        />
+        <button onClick={this.addNewTodo}>add todo</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => {
+  bindActionCreators(todoActions, dispatch);
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps)(TodoList);
---
+++ src/reducers/todos.js
@@ -0,0 +1,14 @@
+export default function todos(state = [], action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          id: Math.random(),
+          text: action.text
+        }
+      ];
+    default:
+      return state;
+  }
+}
